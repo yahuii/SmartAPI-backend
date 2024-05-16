45697692fc24ac23e69ab087490ecdd38a5d9386
顾琴 <870687syh@gmail.com>
2024-05-16 14:14:59 +0800

    添加用户接口关系（用户对于该接口的剩余调用次数、总调用次数）

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -46,3 +46,18 @@ isDelete   tinyint  default 0   not null comment '是否删除'
 )comment'接口信息';
 
 
+-- 用户调用接口关系表
+
+create table if not exists huiapi.user_interface_info(
+`id` bigint not null auto_increment comment'主键'primary key,
+`userId` varchar(256) not null comment '调用用户id',
+`interfaceInfoId` varchar(256) null comment '接口id',
+`totalNum` int default 0 not null comment '总调用次数',
+`leftNum` int default 0 not null comment '剩余调用次数',
+`status` int default 0 not null comment '接口状态(0-正常，1-禁用）',
+`method` varchar(256) not null comment '请求类型',
+createTime datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+updateTime datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment'更新时间',
+isDelete   tinyint  default 0   not null comment '是否删除'
+)comment'接口信息';
+
