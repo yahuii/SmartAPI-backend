f53bc6f1f8ee2b934caa96dd112dbf135474bf06
顾琴 <870687syh@gmail.com>
2024-05-19 16:36:02 +0800

    转成maven父子模块

---
+++ huiapi-common/src/main/java/com/huiapi/common/HuiapiCommonApplication.java
@@ -0,0 +1,13 @@
+package com.huiapi.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuiapiCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuiapiCommonApplication.class, args);
+    }
+
+}
